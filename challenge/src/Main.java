4a45bda0347b74f66b4b50cc2ec1c333937d0385
TiagoLandeiroto <tiagolandeiroto@gmail.com>
2021-08-05 23:26:15 +0100

    Main class

--- challenge/src/Main.java
+++ challenge/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        String input;
+
+        System.out.println("Insira os movimentos que pretende realizar?\nN- Norte\nS- Sul\nO- Oeste\nE- Este");
+
+
+    }
+
+    public static void inputString(){
+
+        String input = sc.nextLine();
     }
 }
